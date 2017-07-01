765db20d3a869e43db6ffe2c3c3cd0b3f8858ed3
Yoshitaka Kondo <bad.skyshadow@gmail.com>
2017-07-01 02:39:50 +0000

    dive09

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-100.times do |n|
+#100.times do |n|
+#  email = Faker::Internet.email
+#  name = "User#{n+1}"
+#  password = "password"
+#  User.create!(email: email,
+#               name: name,
+#               password: password,
+#               password_confirmation: password,
+#               )
+#end
+
+n=1
+while n <= 100
+  # ユーザ作成
   email = Faker::Internet.email
+  name = "User#{n}"
   password = "password"
   User.create!(email: email,
+               name: name,
                password: password,
                password_confirmation: password,
                )
+  # ブログ作成
+  title = "Title#{n}"
+  content = "content"
+  Blog.create(
+    title: title,
+    content: content,
+    user_id: n
+  )
+  n = n + 1
 end
